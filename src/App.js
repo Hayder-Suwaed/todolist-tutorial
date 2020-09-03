d398c95d55bc99cc11d854ffec0676f6a19a14bf
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-03 17:46:25 -0600

    Create and render Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
+import { Header } from "./components/layout/Header";
 
 export const App = () => (
   <div className="App">
-    <header className="App-header">
-      <p>Helllllllllllo</p>
-    </header>
+    <Header />
   </div>
 );
---
+++ src/components/layout/Header.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+
+export const Header = () => <p>I am a header</p>
