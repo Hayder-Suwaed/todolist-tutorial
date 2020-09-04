d64e0eca72356d6c5c6217ea82d1e13583d0701b
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-03 18:14:46 -0600

    Added and render Sidbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Content } from "./components/layout/Content";
 import { Header } from "./components/layout/Header";
 
 export const App = () => (
   <div className="App">
     <Header />
+    <Content />
   </div>
 );
