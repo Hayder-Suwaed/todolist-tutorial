ea697e97faa4048494cf2c601ef2091b82b5f8dc
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-07 23:57:00 -0600

    Styling the Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -9,19 +9,19 @@ import {
 export const Sidebar = () => (
   <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar__generic">
-      <li>
+      <li data-testid="inbox" className="inbox">
         <span>
           <FaInbox />
         </span>
         <span>Inbox</span>
       </li>
-      <li>
+      <li data-testid="today" className="today">
         <span>
           <FaRegCalendar />
         </span>
         <span>Today</span>
       </li>
-      <li>
+      <li data-testid="next_7" className="next_7">
         <span>
           <FaRegCalendarAlt />
         </span>
@@ -35,6 +35,6 @@ export const Sidebar = () => (
       <h2>Projects</h2>
     </div>
     <ul className="sidebar__projects">Projects will be here!</ul>
-    Add Project Component Here!
+    Add Project Component Here! 
   </div>
 );
