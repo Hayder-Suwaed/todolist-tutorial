c14dde7bf1f28a5bc0d18414270f4e3a0c94f9eb
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-07 23:59:45 -0600

    Pass & render the props oof PRojects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Content } from "./components/layout/Content";
 import { Header } from "./components/layout/Header";
+import "./App.scss";
+import { selectedProjectsProvider } from "./context/selected-project-context";
+import { ProjectsProvider } from "./context";
 
 export const App = () => (
-  <div className="App">
-    <Header />
-    <Content />
-  </div>
+  <selectedProjectsProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </selectedProjectsProvider>
 );
