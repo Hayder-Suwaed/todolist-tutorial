44d91ef6037cb99027fdffd0045e4984857caa51
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-07 23:59:20 -0600

    Pass & render the props oof PRojects

---
+++ src/context/index.js
@@ -0,0 +1,19 @@
+import {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjctsValue,
+} from "./projects-context";
+import {
+  SelectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjctValue,
+} from "./selected-project-context";
+
+export {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjctsValue,
+  SelectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjctValue,
+};
