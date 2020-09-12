fd47eaa935f14ad0fe913a179e350a8521ff022e
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-09-11 23:12:31 -0600

    Fix bug of typo

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { Checkbox } from "./Checkbox";
-// import { AddTask } from "./AddTask";
 import { useTasks } from "../hooks";
 import { collatedTasks } from "../constants";
 import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
